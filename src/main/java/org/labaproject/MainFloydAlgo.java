1b24e2354ecb86b8872ee91211fb95f2276bc68f
mpayal2008 <payalppatil.2008@gmail.com>
2023-07-20 10:17:37 -0400

    added code to display station names

--- src/main/java/org/labaproject/MainFloydAlgo.java
+++ src/main/java/org/labaproject/MainFloydAlgo.java
@@ -6,6 +6,7 @@ import org.labaproject.database.configuration.MyBatisConfiguration;
 import org.labaproject.database.dao.StationsDao;
 import org.labaproject.database.dao.StationsDaoImpl;
 import org.labaproject.database.mapper.StationsMapper;
+import org.labaproject.database.model.Cities;
 import org.labaproject.database.model.Stations;
 import org.labaproject.floydalgorithm.DistanceMatrix;
 import org.labaproject.floydalgorithm.ShortestStationDistance;
@@ -25,8 +26,14 @@ public class MainFloydAlgo {
             StationsDao stationsDao = new StationsDaoImpl(sqlSession, stationsMapper);
             List<Stations> stationsList = stationsDao.getAllStations();
 
+          LOGGER.info("Welcome to the California Travel Planner!");
+          LOGGER.info("Stations Table:");
+            for (Stations stations : stationsList) {
+                LOGGER.info(stations.getStationName());
+            }
+
             UserInput userInput = new UserInput();
-            System.out.println("Welcome to the California Travel Planner!");
+
             String sourceStation = userInput.promptCity(("Enter the starting Station: "));
             String destinationStation = userInput.promptCity(("Enter Destination Station: "));
           //  String sc = "Rose Garden";
